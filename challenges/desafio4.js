cb80368e6914953f8988bfbcedc64180ee213c31
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-16 19:49:02 -0300

    fix: corrects challenge 4 indentation

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,6 @@
 db.produtos.find(
   { 
-    $and: [{ vendidos: { $gt: 50 } }, { vendidos: { $lt: 100 } }]
+    $and: [{ vendidos: { $gt: 50 } }, { vendidos: { $lt: 100 } }],
   }, 
-  { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 });
+  { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 }
+);
