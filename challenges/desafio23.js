7d2ed1dfdab9a2b8b9d21d0d41642ae09aa9bdb9
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-16 23:24:35 -0300

    fix: Challenge23 - removes comments

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,10 +1,3 @@
-// 23 - Insira os valores combo e tasty no array tags de todos os sanduíches e aproveite para deixar os valores em ordem alfabética ascendente (A a Z)
-// Para isso, escreva no arquivo desafio23.js duas queries, nesta ordem:
-
-// Crie uma query que faça tanto a inserção dos valores combo e tasty no array tags de todos os sanduíches. Ordene os valores de tags em ordem alfabética ascendente.
-
-// Crie uma query que retorne o nome e tags de todos os documentos.
-
 db.produtos
   .updateMany(
     {},
