fdb50f1acebb7928fc36ef39daad9233133e2b37
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-16 21:12:48 -0300

    fix: corrects challenge 9 trailing comma

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -7,8 +7,8 @@ db.produtos
         $elemMatch: {
           tipo: "calorias",
           quantidade: { $lt: 500 },
-        }
-      }
+        },
+      },
     },
     { nome: 1, _id: 0 },
     );
