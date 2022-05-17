fad68acfa46a2f812289dfc8fda885164b42103f
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-16 22:30:40 -0300

    feat: challenge17 - creates a new called resumoProdutos that contains the fields franquia and totalProdutos

---
+++ challenges/desafio17.js
@@ -0,0 +1,13 @@
+db.resumoProdutos
+  .insertOne(
+    {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.count(),
+    },
+    );
+
+db.resumoProdutos
+  .find(
+    {},
+    { franquia: 1, totalProdutos: 1, _id: 0 },
+    );
